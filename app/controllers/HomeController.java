36ecb18de73c3ba4bad0a587a56187d0b1ec42d6
ganov <ganovelli@gmail.com>
2016-07-22 17:18:52 +0200

    Webjar

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,11 +4,15 @@ import play.mvc.*;
 
 import views.html.*;
 
+import javax.inject.Inject;
+
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+
+    @Inject WebJarAssets webJarAssets;
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -17,7 +21,7 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render(""));
+        return ok(index.render("", webJarAssets));
     }
 
 }
